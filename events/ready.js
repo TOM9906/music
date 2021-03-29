86dfe0ff2e23dc306cb34caba0e33c1ac5c9a2a7
TOM9906 <79160801+TOM9906@users.noreply.github.com>
2021-03-29 20:56:14 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[Gaara Music API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("..Help", { // Change your bot's status here
+  await client.user.setActivity("with tom and hyper", { // Change your bot's status here
     type: "LISTENING", //can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
